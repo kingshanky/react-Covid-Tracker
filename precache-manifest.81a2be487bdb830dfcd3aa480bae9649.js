79175108ba189c976a1c5f41740a37181949a499
Shashank Singh <kingshanky2503@gmail.com>
2022-02-01 12:58:27 +0530

    Updates

---
+++ precache-manifest.81a2be487bdb830dfcd3aa480bae9649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724de846d9628a00e1d74d493b8bff9a",
+    "url": "/react-Covid-Tracker/index.html"
+  },
+  {
+    "revision": "b92539a5fe28f1615d2b",
+    "url": "/react-Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "77d8910171b26715cf57",
+    "url": "/react-Covid-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "b92539a5fe28f1615d2b",
+    "url": "/react-Covid-Tracker/static/js/2.dc20a947.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-Covid-Tracker/static/js/2.dc20a947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d8910171b26715cf57",
+    "url": "/react-Covid-Tracker/static/js/main.695bfd34.chunk.js"
+  },
+  {
+    "revision": "15f1bacd70aacdab511e",
+    "url": "/react-Covid-Tracker/static/js/runtime-main.ec1feb66.js"
+  }
+]);
